e2e5282fcd29f9ff7f79805605b799897633c593
Jeremias Epperlein <jeeree@web.de>
2009-06-15 09:43:35 +0200

    rework inserting in mrows and token

--- kformula/flake/TokenElement.cpp
+++ kformula/flake/TokenElement.cpp
@@ -64,6 +64,7 @@ bool TokenElement::isToken() const {
 
 void TokenElement::layout( const AttributeManager* am )
 {
+    kDebug()<<"Layouting";
     // Query the font to use
     m_font = am->font( this );
 
@@ -89,17 +90,23 @@ void TokenElement::layout( const AttributeManager* am )
     setHeight( m_contentPath.boundingRect().height() );
 }
 
-void TokenElement::insertChild( FormulaCursor* cursor, BasicElement* child )
+bool TokenElement::insertChild( int position, BasicElement* child )
 {
-/*    if( child && child->elementType() == Glyph ) {
-        m_rawString.insert( QChar( QChar::ObjectReplacementCharacter ) );
-        m_glyphs.insert();
+    if( child && child->elementType() == Glyph ) {
+    //    m_rawString.insert( QChar( QChar::ObjectReplacementCharacter ) );
+    //    m_glyphs.insert();
+        return false;
+    } else {
+        return false;
     }
-    else*/ if( !child )
-	//TODO: get the text from child
-        m_rawString.insert( cursor->position(), cursor->inputBuffer() );
-	kDebug() << "-"<<m_rawString << "InputBuffer:"<<cursor->inputBuffer();
 }
+
+
+bool TokenElement::insertText ( int position, const QString& text )
+{
+    m_rawString.insert (position,text);
+}
+
 
 bool TokenElement::setCursorTo(FormulaCursor* cursor, QPointF point) {
     int counter = 0;
